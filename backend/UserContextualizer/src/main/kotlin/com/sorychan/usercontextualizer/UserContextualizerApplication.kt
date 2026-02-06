d7760a76eb3a812665bc823ae5f2ee9d3ae4cc64
Sory-Noroc <sorinnoroc1@gmail.com>
2026-02-07 01:10:58 +0200

    Creating the contextualizer api

---
+++ backend/UserContextualizer/src/main/kotlin/com/sorychan/usercontextualizer/UserContextualizerApplication.kt
@@ -0,0 +1,11 @@
+package com.sorychan.usercontextualizer
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class UserContextualizerApplication
+
+fun main(args: Array<String>) {
+    runApplication<UserContextualizerApplication>(*args)
+}
---
+++ backend/UserContextualizer/src/test/kotlin/com/sorychan/usercontextualizer/UserContextualizerApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.sorychan.usercontextualizer
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class UserContextualizerApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
